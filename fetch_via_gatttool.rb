cb5f5d4c3f737b9e10b4a3b83cda0cefafe2ee7c
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2021-01-04 18:03:06 +0900

    notify

--- fetch_via_gatttool.rb
+++ fetch_via_gatttool.rb
@@ -50,13 +50,34 @@ class OmronSensor
       nil
     end
   end
+
+  def notify_sensing_data(queue)
+    cmd = "sudo gatttool -b #{@addr} -t random --char-write-req -a 0x5a -n 0100 --listen"
+    begin
+      io = IO.popen(cmd, 'r')
+      while line = io.gets
+        if /value\: (.*)/ =~ line
+          it = [$1.split.join('')].pack("H*").unpack("Cssslsss")
+          queue.push(it)
+        end
+      end
+    rescue
+      io.close
+    end
+  end
 end
 
 if __FILE__ == $0
   require 'pp'
+  require 'thread'
   o = OmronSensor.new
   v1 = o.latest_sensing_data
   v2 = o.latest_calculation_data
   pp o.data_to_hash(v1, OmronSensor::Tlatest_sensing_data)
   pp o.data_to_hash(v2, OmronSensor::Tlatest_calculation_data)
+  queue = Queue.new
+  Thread.new {o.notify_sensing_data(queue)}
+  while true
+    pp o.data_to_hash(queue.pop, OmronSensor::Tlatest_sensing_data)
+  end
 end
